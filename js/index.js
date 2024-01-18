db133e0bfa0ae8ffc40de684f893d0b6639ea1d5
Neeha-A <139718422+Neeha-A@users.noreply.github.com>
2024-01-18 19:11:39 +0000

    hw

---
+++ js/index.js
@@ -0,0 +1,32 @@
+console.log("your file is loaded");
+
+$(".daydreaming").on("mouseenter", function(event) {
+    $(this).addClass("hover");
+  });
+  
+  $(".daydreaming").on("mouseleave", function(event) {
+    $(this).removeClass("hover");
+  });
+
+
+
+
+  
+  $(".gaming").on("mouseenter", function(event) {
+    $(this).addClass("hover");
+  });
+  
+  $(".gaming").on("mouseleave", function(event) {
+    $(this).removeClass("hover");
+  });
+
+
+
+
+  $(".story").on("mouseenter", function(event) {
+    $(this).addClass("hover");
+  });
+  
+  $(".story").on("mouseleave", function(event) {
+    $(this).removeClass("hover");
+  });
